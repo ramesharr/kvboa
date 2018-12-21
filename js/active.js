e7dfdb0ddd55f7b9ec38eecacdd7ac3ad799e8bd
rameshsoft1 <38454779+rameshsoft1@users.noreply.github.com>
2018-12-21 08:01:31 +0530

    active.js form
    added form

--- js/active.js
+++ js/active.js
@@ -144,5 +144,30 @@
     $('#closebtn2').on('click', function () {
         $('.add-banner2').fadeOut(500);
     });
-    $('header a').removeAttr('href');
+    /*$('header a').removeAttr('href');*/
+
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }else{
+                if(form.id == 'form-1'){
+                    event.preventDefault();
+                    event.stopPropagation();
+                    $(".reg-form-1").hide();
+                    $(".reg-form-2").show();
+                }
+
+            }
+            
+            form.classList.add('was-validated');
+          }, false);
+        });
+      }, false);
+
 })(jQuery);
